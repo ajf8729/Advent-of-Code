0a2178ed9906020c35e7e01f211dfbf4fa530cd5
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2023-12-06 01:02:29 -0500

    Minor optimization to d6p2

--- 2023/06/p2.ps1
+++ 2023/06/p2.ps1
@@ -8,13 +8,19 @@ $time = ($data[0].Split(':')[1].trim() -replace '\s+', '')
 $distance = ($data[1].Split(':')[1].trim() -replace '\s+', '')
 $heldtime = 1
 $wins = 0
+$wintripped = $false
+$endnow = $false
 
-while ($heldtime -lt $time) {
+while ($heldtime -lt $time -and !$endnow) {
     $speed = $heldtime
     $timeleft = $time - $heldtime
     $distancetraveled = $timeleft * $speed
     if ($distancetraveled -gt $distance) {
         $wins++
+        $wintripped = $true
+    }
+    if ($true -eq $wintripped -and $distancetraveled -le $distance) {
+        $endnow = $true
     }
     $heldtime++
 }

f96d5104293adb8ec9f5ee1a57dcbd35816e0e63
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2024-12-04 01:09:40 -0500

    fixing z-nerd's xmas tree

--- 2024/04/p1.ps1
+++ 2024/04/p1.ps1
@@ -11,18 +11,18 @@ for ($i = 0; $i -lt $d.Length; $i++) {
                     }
                 }
             }
-            #down
-            if (($i + 1) -lt $d.Length -and $d[$i + 1][$j] -eq 'M') {
-                if (($i + 2) -lt $d.Length -and $d[$i + 2][$j] -eq 'A') {
-                    if (($i + 3) -lt $d.Length -and $d[$i + 3][$j] -eq 'S') {
-                        $xmas++
-                    }
-                }
-            }
             #left
             if (($j - 1) -ge 0 -and $d[$i][$j - 1] -eq 'M') {
                 if (($j - 2) -ge 0 -and $d[$i][$j - 2] -eq 'A') {
                     if (($j - 3) -ge 0 -and $d[$i][$j - 3] -eq 'S') {
+                        $xmas++
+                    }
+                }
+            }
+            #down
+            if (($i + 1) -lt $d.Length -and $d[$i + 1][$j] -eq 'M') {
+                if (($i + 2) -lt $d.Length -and $d[$i + 2][$j] -eq 'A') {
+                    if (($i + 3) -lt $d.Length -and $d[$i + 3][$j] -eq 'S') {
                         $xmas++
                     }
                 }
@@ -43,18 +43,18 @@ for ($i = 0; $i -lt $d.Length; $i++) {
                     }
                 }
             }
-            #up-right
-            if (($i - 1) -ge 0 -and ($j + 1) -lt $d[$i].Length -and $d[$i - 1][$j + 1] -eq 'M') {
-                if (($i - 2) -ge 0 -and ($j + 2) -lt $d[$i].Length -and $d[$i - 2][$j + 2] -eq 'A') {
-                    if (($i - 3) -ge 0 -and ($j + 3) -lt $d[$i].Length -and $d[$i - 3][$j + 3] -eq 'S') {
-                        $xmas++
-                    }
-                }
-            }
             #down-left
             if (($i + 1) -lt $d.Length -and ($j - 1) -ge 0 -and $d[$i + 1][$j - 1] -eq 'M') {
                 if (($i + 2) -lt $d.Length -and ($j - 2) -ge 0 -and $d[$i + 2][$j - 2] -eq 'A') {
                     if (($i + 3) -lt $d.Length -and ($j - 3) -ge 0 -and $d[$i + 3][$j - 3] -eq 'S') {
+                        $xmas++
+                    }
+                }
+            }
+            #up-right
+            if (($i - 1) -ge 0 -and ($j + 1) -lt $d[$i].Length -and $d[$i - 1][$j + 1] -eq 'M') {
+                if (($i - 2) -ge 0 -and ($j + 2) -lt $d[$i].Length -and $d[$i - 2][$j + 2] -eq 'A') {
+                    if (($i - 3) -ge 0 -and ($j + 3) -lt $d[$i].Length -and $d[$i - 3][$j + 3] -eq 'S') {
                         $xmas++
                     }
                 }

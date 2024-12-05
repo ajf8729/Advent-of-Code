ec9574aaae372d969d9f6744fe142f8d0076f901
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2024-12-05 13:51:32 -0500

    day 5 part 1 complete!

---
+++ 2024/05/p1.ps1
@@ -0,0 +1,25 @@
+$d = Get-Content d.txt
+$rules = (Select-String -InputObject $d -Pattern '\d\d\|\d\d' -AllMatches).Matches.Value
+$updates = $d -replace '\d\d\|\d\d', '' | Where-Object { $_.Trim() -ne ''}
+$p1 = 0
+foreach ($update in $updates) {
+    $pages = $update.Split(',')
+    $length = $pages.Length
+    for ($i = 0; $i -lt $length - 1; $i++) {
+        $bad = $false
+        for ($j = $i + 1; $j -lt $length; $j++) {
+            if ("$($pages[$j])|$($pages[$i])" -in $rules) {
+                $bad = $true
+                break
+            }
+        }
+        if ($bad) {
+            break
+        }
+    }
+    if (!$bad) {
+        $p1 += $pages[[math]::floor($pages.Length / 2)]
+        $bad = $false
+    }
+}
+$p1

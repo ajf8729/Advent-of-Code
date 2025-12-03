4144d02c142b78fe47c43f774a61e6f92308a4c7
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2025-12-03 10:07:58 -0500

    🤪

--- 2025/02/p2.ps1
+++ 2025/02/p2.ps1
@@ -13,45 +13,48 @@ function Get-PossibleFactors {
     return $possiblefactors | Sort-Object
 }
 
-$data = Get-Content .\input.txt
+$data = Get-Content .\z.txt
 $data = $data.Split(',')
 $invalid = 0
+$counter = 0
 foreach ($item in $data) {
-    "CURRENTLY WORKING ON: $item"
+    $counter++
+    "PROCESSING: $counter out of 38"
     [bigint]$start = $item.Split('-')[0]
     [bigint]$end = $item.Split('-')[1]
     $difference = $end - $start
     #"difference = $difference"
     for ($i = 0; $i -lt $difference + 1; $i++) {
         [int]$length = $start.ToString().Length
-        #"length = $length"
-        #"start = $start"
-        $factors = Get-PossibleFactors -Value $length
-        #"factors = $factors"
-        foreach ($factor in $factors) {
-            #"start = $start"
-            #"factor = $factor"
+        if ($length -gt 1) {
             #"length = $length"
-            $substrings = @()
-            for ($j = 0; $j -lt $length; $j += $factor) {
-                #"j = $j"
+            #"start = $start"
+            $factors = Get-PossibleFactors -Value $length
+            #"factors = $factors"
+            foreach ($factor in $factors) {
+                #"start = $start"
                 #"factor = $factor"
-                $substrings += $start.ToString().Substring($j, $factor)
-                #"substrings = $substrings"
-                #pause
-            }
-            if ($substrings | Group-Object | Where-Object { $_.Count -eq $substrings.length }) {
-                $invalid += $start
-                "INVALID: $start"
-                if ($start.ToString().ToCharArray() | Group-Object | Where-Object { $_.Count -eq $start.ToString().Length }) {
-                    break
+                #"length = $length"
+                $substrings = @()
+                for ($j = 0; $j -lt $length; $j += $factor) {
+                    #"j = $j"
+                    #"factor = $factor"
+                    $substrings += $start.ToString().Substring($j, $factor)
+                    #"substrings = $substrings"
+                    #pause
                 }
+                if ($substrings | Group-Object | Where-Object { $_.Count -eq $substrings.length }) {
+                    $invalid += $start
+                    "INVALID: $substrings"
+                    if ($start.ToString().ToCharArray() | Group-Object | Where-Object { $_.Count -eq $start.ToString().Length }) {
+                        break
+                    }
+                }
+                #Pause
+            
             }
-            #Pause
         }
         $start = $start + 1
     }
 }
-#'correct = 4174379265'
-#"mine =    $invalid"
 $invalid
